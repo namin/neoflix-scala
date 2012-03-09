48c3b3232259006b90882a910ee6df1c6e49eedc
Nada Amin <namin@alum.mit.edu>
2012-03-10 00:03:44 +0100

    a first try at using gremlin-scala

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,9 +8,13 @@ import org.neo4j.graphdb.DynamicRelationshipType
 import org.neo4j.graphdb.GraphDatabaseService
 import org.neo4j.rest.graphdb.RestGraphDatabase
 
+import com.tinkerpop.blueprints.pgm.impls.neo4j.Neo4jGraph
+import com.tinkerpop.gremlin.scala._
+
 object Application extends Controller {
   val gds: GraphDatabaseService = new RestGraphDatabase(
     Option.apply(System.getenv("NEO4J_REST_URL")).getOrElse("http://localhost:7474/db/data"), System.getenv("NEO4J_LOGIN"), System.getenv("NEO4J_PASSWORD"))
+  val g = new Neo4jGraph(gds)
   val me = gds.getReferenceNode()
   if (!me.hasProperty("name")) {
     me.setProperty("name", "I")
@@ -20,11 +24,12 @@ object Application extends Controller {
   }
 
   def index = Action {
-    val me = gds.getNodeById(0)
-    val rels = me.getRelationships(DynamicRelationshipType.withName("love"))
-    val rel = rels.head
-    val you = rel.getEndNode()
-    val message = me.getProperty("name") + " " + rel.getType().name() + " " + you.getProperty("name")
+//    val me = gds.getNodeById(0)
+//    val rels = me.getRelationships(DynamicRelationshipType.withName("love"))
+//    val rel = rels.head
+//    val you = rel.getEndNode()
+//    val message = me.getProperty("name") + " " + rel.getType().name() + " " + you.getProperty("name")
+	val message = g.v(0).getProperty("name") + " " + g.v(0).outE.label.toIterable.head + " " + g.v(0).out.property("name").toIterable.head
     Ok(views.html.index(message))
   }
   
--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,9 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "org.neo4j" % "neo4j-rest-graphdb" % "1.6"
+      "org.neo4j" % "neo4j-rest-graphdb" % "1.6",
+      "com.tinkerpop.gremlin" % "gremlin-scala" % "1.5",
+      "com.tinkerpop.blueprints" % "blueprints-neo4j-graph" % "1.2"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
