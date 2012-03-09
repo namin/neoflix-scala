06a6fa9cc3bfe9eb17e79116c91c460f8082dfe5
Nada Amin <namin@alum.mit.edu>
2012-03-09 20:20:10 +0100

    test: i love you

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,12 +3,29 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-import com.typesafe.config._
+import scala.collection.JavaConversions.iterableAsScalaIterable
+import org.neo4j.graphdb.DynamicRelationshipType
+import org.neo4j.graphdb.GraphDatabaseService
+import org.neo4j.rest.graphdb.RestGraphDatabase
 
 object Application extends Controller {
+  val gds: GraphDatabaseService = new RestGraphDatabase(
+    System.getenv("NEO4J_REST_URL"), System.getenv("NEO4J_LOGIN"), System.getenv("NEO4J_PASSWORD"))
+  val me = gds.getReferenceNode()
+  if (!me.hasProperty("name")) {
+    me.setProperty("name", "I")
+    val you = gds.createNode()
+    you.setProperty("name", "you")
+    me.createRelationshipTo(you, DynamicRelationshipType.withName("love"))
+  }
+
   def index = Action {
-    val conf = ConfigFactory.load()
-    Ok(views.html.index(conf.getString("neo4j.url")))
+    val me = gds.getNodeById(0)
+    val rels = me.getRelationships(DynamicRelationshipType.withName("love"))
+    val rel = rels.head
+    val you = rel.getEndNode()
+    val message = me.getProperty("name") + " " + rel.getType().name() + " " + you.getProperty("name")
+    Ok(views.html.index(message))
   }
   
 }
